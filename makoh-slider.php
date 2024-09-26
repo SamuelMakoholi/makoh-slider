265f9f5a9897e2d0c88b122adf1e6b1ba734db12
Samuel Makoholi <samuelmakoh@gmail.com>
2024-09-26 18:00:27 +0200

    slider files

--- makoh-slider.php
+++ makoh-slider.php
@@ -1,1 +1,38 @@
-Swiper Slider
+<?php
+
+/**
+ * Plugin Name: Makoh Slider
+ * Description: Makoh Slider Plugin is a best for univerisities who want to show their latest posts on the front page but you can put anywhere you want. It has pixel perfect design, mobile responsive editing, and more. Get started now!
+ * Plugin URI: https://github.com/SamuelMakoholi/makoh-slider
+ * Author: Samuel Makoholi
+ * Version: 1.0.0
+ *
+ * Text Domain: makoh-slider
+ *
+ * @package Makoh Slider
+ *
+ * Makoh Slider is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Makoh Slider is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Include the class responsible for enqueuing assets
+require_once plugin_dir_path(__FILE__) . 'makoh-assets.php';
+
+//initialize the assets
+new Makoh_Slider_Assets();
+
+
+
+
+?>
